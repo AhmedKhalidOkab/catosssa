5b433a47987d7abea802e1e8ffe30ca80166aba0
AhmedKhalidOkab <https://github.com/AhmedKhalidOkab>
2023-02-22 21:43:45 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/cattoosa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cattoosa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constant/colors.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const mainColor = Color.fromRGBO(231, 15, 44, 1);
+  static const primaryText = Color(0xff171D5B);
+  static const secondaryText = Color(0xff57636C);
+  static const colorRed = Color(0xffDC5224);
+
+  static Color unslectedIconColor = Colors.black.withOpacity(0.5);
+  static const backgroundColor = Color(0xffF6F6F6);
+  static const colorGrey = Color(0xff00000029);
+  static const colorgrey2 = Color(0xffEBEDEE);
+  static const colorBlack = Color(0xff000000);
+  static const textColor = Color.fromRGBO(36, 31, 31, 1);
+  static const colorBlue = Color(0xff0093F5);
+  static const colorDeepgrey = Color(0xff68696A);
+
+  static const dividerColor = Color(0xff707070);
+  static const colorGreen = Color(0xff07BF1F);
+  static const descriptionColor = Color(0xffF3F3F3);
+  static const borderLightColor = Color(0xff0000001A);
+  static const greenDeepColor = Color(0xff019F1B);
+  static const dotsColor = Color(0xffEBEDEE);
+  static const textFeildColor = Color(0xffF6F6F6);
+  static const iconColorGrey = Color(0xff636363);
+  static const hinttext = Color(0xff787878);
+  static const textColor2 = Color(0xff3B3B3B);
+  static const colorWhite = Color(0xffFFFFFF);
+  //Botom color
+  static const BottomColor1 = Color.fromRGBO(255, 144, 159, 1);
+}
+
+TextStyle headingStyle =
+    TextStyle(fontFamily: 'bader_al gordabia bader', color: MyColors.mainColor);
